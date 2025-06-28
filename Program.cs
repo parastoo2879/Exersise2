92591f9102ffb3e280435afe9f83783ad0527762
Parastoo Ghanbarzade <parastoo.ghanbarzade@gmail.com>
2025-06-28 07:43:30 -0700

    comment cleanup

--- Program.cs
+++ Program.cs
@@ -4,13 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //for (int i = 0; i<=20;i++)
-            //{
-            //    Console.Write($"Number {i +1} ");
-            //    if (i % 2 == 0) Console.Write($"{i}  \n");
-
-            //}
-
             int  i = 0;
             int count = 1;
             while (i<=20)
